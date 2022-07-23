b176026c8e2189d7b55230cf63f36088d9af4f9c
Edgar Munoz <munozedgar510@gmail.com>
2022-07-22 20:53:22 -0700

    key

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import {useLoadScript } from @react-google-maps/api
 export default function Home() {
     const {isLoaded} = useLoadScript({
-      googleMapsApiKey = "AIzaSyCIFeFzI08oLUYzy3blkAdkdlMHGG5CEcc",
+      googleMapsApiKey = "DO NOT EXPOSE KEY TO PUBLIC",
     })
     return (
         <h1>Home Page</h1>
